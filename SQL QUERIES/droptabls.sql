b460bed56951b27c36b838ab8c2e894cdcd3b3c7
linaticcode <ss21eeb0a59@student.nitw.ac.in>
2023-07-17 21:15:09 +0530

    update changes

--- SQL QUERIES/droptabls.sql
+++
@@ -1,7 +0,0 @@
-drop table if exists ig_clone.comments;
-drop table if exists ig_clone.follows;
-drop table if exists ig_clone.likes;
-drop table if exists ig_clone.photo_tags;
-drop table if exists ig_clone.photos;
-drop table if exists ig_clone.tags;
-drop table if exists ig_clone.users;
