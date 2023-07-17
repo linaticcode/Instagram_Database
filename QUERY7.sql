6fc3a6108becc69b7c8c002c7a9443a1758d9988
linaticcode <ss21eeb0a59@student.nitw.ac.in>
2023-07-17 21:13:20 +0530

    update changes

--- QUERY7.sql
+++
@@ -1,9 +0,0 @@
-SELECT username, 
-       Count(*) AS num_likes 
-FROM   users 
-       INNER JOIN likes 
-               ON users.id = likes.user_id 
-GROUP  BY likes.user_id 
-HAVING num_likes = (SELECT Count(*) 
-                    FROM   photos); 
-                    
--- insert tags.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO tags(tag_name) VALUES ('sunset'), ('photography'), ('sunrise'), ('landscape'), ('food'), ('foodie'), ('delicious'), ('beauty'), ('stunning'), ('dreamy'), ('lol'), ('happy'), ('fun'), ('style'), ('hair'), ('fashion'), ('party'), ('concert'), ('drunk'), ('beach'), ('smile');
